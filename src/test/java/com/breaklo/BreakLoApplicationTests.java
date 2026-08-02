9b5d9cd1fa1221ecd073caf4029d3b699389571f
lucerodelossantosjasso <lucero.dlsan@gmail.com>
2026-08-02 13:21:50 -0600

    Configurar estructura inicial del proyecto BreakLo

---
+++ src/test/java/com/breaklo/BreakLoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breaklo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakLoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
